9c80f3636de9c5d8ac276fbc1f830a73308a8388
glsoares <46654940+glsoares@users.noreply.github.com>
2024-03-07 16:54:26 -0500

    Update BCG_payload_parsing.ino
    changes made to reset function and comments added to be sent to murrata

--- BCG_payload_parsing.ino
+++ BCG_payload_parsing.ino
@@ -3,12 +3,14 @@
 byte SOF = 0;
 byte length = 0;
 byte inBuffer[2];
-byte reset_response[6];
+byte reset_response[10] = {0};
 int data;
-int debug=1;
+int debug = 1;
+int sent = 0;
+byte reset[] = {0xFD,0x00,0x02,0x01,0x00,0xFE}; // reset command
 
 void setup() {
-  // put your setup code here, to run once:
+  // setup code
   pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
  
   digitalWrite(LED_BUILTIN, HIGH);    // switch on LED pin
@@ -22,34 +24,11 @@ void setup() {
   while(!Serial1);
   Serial.println("Ready");
 
-
-  byte reset[] = {0xFE,0x00,0x01,0x00,0x02,0xFD}; // reset command
-  while (Serial1.available() > 0) {
-
-    if(Serial.availableForWrite() > 0){
-      Serial.write(reset,sizeof(reset)); //send reset command
-      digitalWrite(LED_BUILTIN, HIGH); // set LED pin on
-      break;
-    }
-    //read BCG response
-    Serial1.readBytesUntil('\n',reset_response,sizeof(reset_response));
-    
-    if (debug==1){
-      for (int i=0;i<sizeof(reset_response);i++){
-        Serial.print(reset_response[i],HEX);
-        Serial.print(" ");
-      }
-      Serial.print('\n');
-    }
-    digitalWrite(LED_BUILTIN, LOW);
-      
-    if(reset_response[5]==0){break;} // indicates successful reset of BCG
-
 }
 
 void loop() {
 
-  while (Serial1.available() > 0) {
+ while (Serial1.available() > 0) {
     Serial1.readBytesUntil('\n',inBuffer,2); // read first 2 bits for SOF and length
     if(inBuffer[0]==0xFE){ // if SOF detected, continue
       if (debug==1){
@@ -64,41 +43,67 @@ void loop() {
       data = Serial1.readBytesUntil('\n',dataFrame,length); // read data frame
 
       for (int i=0;i<data;i++){
-        Serial.print(dataFrame[i],HEX);
+        Serial.print(dataFrame[i],HEX); //print BCG data to serial port
         Serial.print(" ");
       }
       Serial.print('\n');
-      // define BCG payload
-      byte ID = dataFrame[0];
-      uint16_t type = dataFrame[1]<<8 | dataFrame[2];
+      // define BCG payload as per binary protocol specification document
+      byte type = dataFrame[0];
+      uint16_t ID = dataFrame[1]<<8 | dataFrame[2];
       uint32_t time = dataFrame[6]<<24 |dataFrame[5]<<16 | dataFrame[4]<<8 | dataFrame[3];
       uint32_t HR = dataFrame[10]<<24 |dataFrame[9]<<16 | dataFrame[8]<<8 | dataFrame[7];
       uint32_t SV = dataFrame[14]<<24 |dataFrame[13]<<16 | dataFrame[12]<<8 | dataFrame[11];
       uint32_t RR = dataFrame[18]<<24 |dataFrame[17]<<16 | dataFrame[16]<<8 | dataFrame[15];
       uint32_t HRV = dataFrame[22]<<24 |dataFrame[21]<<16 | dataFrame[20]<<8 | dataFrame[19];
-      
       uint32_t fft_output = dataFrame[26]<<24 |dataFrame[25]<<16 | dataFrame[24]<<8 | dataFrame[23];
       uint32_t status = dataFrame[30]<<24 |dataFrame[29]<<16 | dataFrame[28]<<8 | dataFrame[27];
       uint32_t B2B = dataFrame[34]<<24 |dataFrame[33]<<16 | dataFrame[32]<<8 | dataFrame[31];
       uint32_t B2B1 = dataFrame[38]<<24 |dataFrame[37]<<16 | dataFrame[36]<<8 | dataFrame[35];
       uint32_t B2B2 = dataFrame[42]<<24 |dataFrame[41]<<16 | dataFrame[40]<<8 | dataFrame[39];
       uint32_t FCS = dataFrame[43];
-      Serial.print("TIME ");
-      Serial.println(time);
+      Serial.print("HR="); //print heart rate
+      Serial.println(HR);
       }
 
   }
+
+  // the code below is an attempt for sending a reset command to the BCG
+  /*
+  while(Serial1.available())  {
+    if(sent==0) {
+      Serial1.write(reset,sizeof(reset));
+      sent = 1;
+      Serial.println("Reset Command Sent");
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+      Serial1.readBytesUntil(0xFE,reset_response,sizeof(reset_response)); // read first 2 bits for SOF and length
+  
+    //if(reset_response[1]==0x01){ // if ID detected, continue
+      if (reset_response[1]==0x01)  {
+        for (int i=0;i<sizeof(reset_response);i++){
+          Serial.print(reset_response[i],HEX);
+          Serial.print(" ");
+        }
+        Serial.print('\n');
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+  //}  
+  }*/
 }
 
+
 int resetBCG(){
+    pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
+
   byte reset[] = {0xFE,0x00,0x01,0x00,0x02,0xFD}; // reset command
   while (Serial1.available() > 0) {
-
     if(Serial.availableForWrite() > 0){
       Serial.write(reset,sizeof(reset)); //send reset command
       digitalWrite(LED_BUILTIN, HIGH); // set LED pin on
       break;
     }
+  }
+  while (Serial1.available() > 0) {
     //read BCG response
     Serial1.readBytesUntil('\n',reset_response,sizeof(reset_response));
     
