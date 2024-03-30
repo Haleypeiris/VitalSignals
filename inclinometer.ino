9c90e18665edc9fde423528583f38764dbbdec2e
Natalie Hawrylak <nat.hawrylak@gmail.com>
2024-03-29 21:45:51 -0400

    Sampling rate changed
    Now increased from 4hz to 10hz

--- inclinometer.ino
+++ inclinometer.ino
@@ -8,13 +8,16 @@
 #include <WiFi.h>
 
 //wifi
-const char* ssid     = "NatalieiPhone";
-const char* password = "nataliehaw";
+//const char* ssid     = "NatalieiPhone";
+//const char* password = "nataliehaw";
+const char* ssid     = "BELL950";
+const char* password = "6DC7C2EC6411";
 WiFiServer server(80);
 WiFiClient client;
 int HTTP_PORT = 80;
 String HTTP_METHOD = "GET";
-char HOST_NAME[] = "172.20.10.2";  //change to computer's IP address //specific to Natalie's set up
+char HOST_NAME[] = "192.168.2.123"; //BELL950 one
+//char HOST_NAME[] = "172.20.10.2";  //change to computer's IP address //specific to Natalie's set up
 //String PATH_INSERT = "/capstone/inclinometer/public/insert_to_sql.php"; //specific to Natalie's set up
 //String PATH_INSERT = "/capstone/inclinometer/public/staff/insert_values.php";
 String PATH_INSERT = "/capstone/bcgparsing/public/staff/insert_values.php";
@@ -29,7 +32,7 @@ double yAccArray[400];
 String yAccString; 
 //uint32_t period = 10000L;
 int count;
-uint32_t period = 10000L; //5*6000L was supposed to be 5min
+uint32_t period = 20000L; //5*6000L was supposed to be 5min
 //Default SPI chip/slave select pin is D10
 
 // Need the following define for SAMD processors
@@ -68,7 +71,7 @@ void sendGET(char HOST_NAME[]) //client function to send/receive GET request dat
 }  // end of sendget
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //normally 9600 - no difference in number of points sent at 115200
   //scl
   delay(2000); //SAMD boards may need a long time to init SerialUSB
   Serial.println("Reading basic Accelerometer values from SCL3300 Inclinometer");
@@ -97,8 +100,6 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.println(".");
-        //WiFi.begin(ssid, password); //aka try again?
-
     }
 
     Serial.println("");
@@ -110,7 +111,6 @@ void setup() {
 }
 
 void loop() {
-  //Serial.println("trying");
   yAccString = String('"');
   count = 0;
   for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
@@ -123,33 +123,22 @@ void loop() {
       //Serial.print("Y Accelerometer: ");
       yAcc = double(inclinometer.getCalculatedAccelerometerY()*100000); //need as float for get request
       //Serial.print(yAcc);
-      //Serial.println();
       yAccString += String(yAcc);
-      yAccString += "x";
-
-      //cute little sendGET code for http stuff
-      //float_rr = String(radar.breath_rate);
-      //queryString = "?measure="+float_hr+"&rr="+float_rr;
+      yAccString += ",";
       xAcc = double(inclinometer.getCalculatedAccelerometerX()*100000); //just to send a second value rn
-      //queryString = "?yAcc="+yAcc+"&rr="+xAcc; //maybe just leave as is bc 
-      //Serial.println(queryString);
-      //delay(1000);
-      //sendGET(HOST_NAME);
       /*Serial.print(inclinometer.getCalculatedAccelerometerY());
       Serial.print("\t");
       Serial.print("Z Accelerometer: ");
       Serial.println(inclinometer.getCalculatedAccelerometerZ());*/
-      delay(250); //Allow a little time to see the output
+      delay(100); //Allow a little time to see the output
     } else inclinometer.reset();
     count++;
   }
   yAccString += '"';
+  //yAccString.replace("-","n");
   Serial.println(yAccString);
   Serial.println(count);
   queryString = "?yAcc="+yAcc+"&rr="+xAcc+"&yAccString="+yAccString;
   //queryString = "?yAcc="+yAcc+"&rr="+xAcc;
   sendGET(HOST_NAME);
 }
-
-//testing value
-//localhost/capstone/bcgparsing/public/staff/insert_values.php?yAcc=3517&rr=2308&yAccString='-3525.00x-3391.67x-3491.67x-3383.33x-3566.67x-3375.00x-3408.33x-3516.67x-3608.33x-3458.33x-3541.67x-3575.00x-3316.67x-3416.67x-3458.33x-3608.33x-3291.67x-3575.00x-3533.33x-3433.33x-3391.67x-3541.67x-3441.67x-3408.33x-3466.67x-3441.67x-3475.00x-3450.00x-3691.67x-3500.00x-3500.00x-3550.00x-3475.00x-3316.67x-3433.33x-3483.33x-3408.33x-3441.67x-3508.33x-3516.67x'
