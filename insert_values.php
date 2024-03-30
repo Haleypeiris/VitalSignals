f03d37b6e5b436c0611e30b43dede94f83f4459e
Natalie Hawrylak <nat.hawrylak@gmail.com>
2024-03-29 21:51:11 -0400

    Added PHP file
    Sends data from arduino to the database via this code with the MySQL request

---
+++ insert_values.php
@@ -0,0 +1,44 @@
+<?php require_once('../../private/initialize.php'); ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<?php
+//echo url_for('/staff/insert_values.php?measure='. $_GET["measure"]);
+if(isset($_GET["yAcc"])) {
+   $yAcc = $_GET["yAcc"]; // get temperature value from HTTP GET
+   //echo $yAcc;
+   $rr = $_GET["rr"];
+   //echo $rr;
+   $yAccString = strval($_GET["yAccString"]);
+   $yAccText = mysqli_real_escape_string($db, $yAccString);
+   //$yAccString = mysqli_real_escape_string($db, $_GET["yAccString"]);
+   echo $yAccString;
+   /*$distance = $_GET["distance"];
+   if $distance>0{
+
+   }*/
+   $patient_id = 1;
+   //$sql = "INSERT INTO acceleration (patient_id, y_acc, x_acc, z_acc, time_stamp, acc_array) VALUES ($patient_id, $yAcc, $rr, 1290.00, NOW(), $yAccString)";
+   $sql = "INSERT INTO giselle20 (patient_id, y_acc, x_acc, z_acc, time_stamp, acc_array) VALUES ($patient_id, $yAcc, $rr, 1290.00, NOW(), $yAccString)";
+   //$sql = "INSERT INTO radar_sensor (patient_id, heart_rate, respiratory_rate, bed_status, time_stamp) VALUES ($patient_id, $hr, $rr, $distance, NOW())";
+
+   //only relevant to see php page online
+   
+   if (mysqli_query($db,$sql) === TRUE) { //aka if true
+      //echo "Value from radar: " . $measure;
+      echo "Current heart rate: ";
+      //echo $heartrate;
+      $query = "SELECT * FROM radar_sensor ORDER BY id DESC LIMIT 1";
+      $result = mysqli_query($db, $query); 
+      $row = mysqli_fetch_array($result); //array
+      echo $row['heart_rate']; //displays only heart rate; do print_r($row) to see whole row I think
+      echo " bpm" . "<br />";
+      echo "Timestamp: " . date("Y-m-d h:i:sa"). "<br />";
+   } else {
+      echo "Error: " . $sql . " => " . $db->error;
+   }
+   
+} else {
+   echo "measurement is not set - fix it";
+}
+?>
+<!--<?php include(SHARED_PATH . '/staff_footer.php'); ?>-->
